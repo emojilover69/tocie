b42070dfcdec60ad78366c0899a4710ebe7bcc1f
emojilover69 <116074183+emojilover69@users.noreply.github.com>
2024-01-19 11:34:25 +0000

    Add counter rewriting function

---
+++ netlify/edge-functions/counter.js
@@ -0,0 +1,28 @@
+import { Client, fql } from 'fauna';
+
+async function getCounter() {
+    const client = new Client({
+        secret: Netlify.env.get("FAUNA_DB_SECRET")
+    });
+
+    try {    
+        const q = fql`counters.byId("1") {count}`;
+        const resp = await client.query(q);
+        client.close();
+        return resp.data.count;
+    } catch(e) {
+        client.close();
+        return "unknown";
+    }
+}
+
+export default async function handler(req, context) {
+    const response = await context.next();
+    const page = await response.text();
+
+    const counterTemplate = /{{COUNTER}}/;
+    const updatedPage = page.replace(counterTemplate, await getCounter());
+    return new Response(updatedPage, response);
+}
+
+const const config = { path: '/' };
